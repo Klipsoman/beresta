e2c70446f50db1dce43554ad4f4a7d99878fc74a
Sneik <Sneik92@ya.ru>
2023-10-01 13:34:05 +0300

    init

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
