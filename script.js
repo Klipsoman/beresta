a6cbe1b9344ff9616e1fa20a65feed525a01cc23
Sneik <Sneik92@ya.ru>
2023-10-01 19:42:21 +0300

    add scripts, add modal form, add mobile-menu

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
+// Booking modal form
+const bookingBtn = document.getElementById('booking-btn');
+const modalForm = document.getElementById('modal-form');
+const modalFormCancelBtn = document.getElementById('modal-form__cancel-btn');
 
+bookingBtn.addEventListener('click', () => {
+    document.body.style.paddingRight = '17px';
+    document.body.style.overflow = 'hidden';
+    modalForm.style.display = 'flex';
+});
+modalFormCancelBtn.addEventListener('click', () => {
+    document.body.style.paddingRight = '0';
+    document.body.style.overflow = 'auto';
+    modalForm.style.display = 'none';
+});
 
-menuBtn.addEventListener('click', function(){
-    menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
-})
+// Scroll to #ankor links
+const headerMenuLinks = document.querySelectorAll('#header__nav .header__link');
+const footerMenuLinks = document.querySelectorAll('.footer .footer__link');
+
+[...headerMenuLinks, ...footerMenuLinks].forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = link.getAttribute('href')?.slice(1);
+        const targetElement = document.getElementById(target);
+
+        if (!targetElement) return;
+
+        const header = document.getElementById('header');
+
+        window.scrollTo(0, targetElement.offsetTop - header.clientHeight);
+    });
+});
+
+// Mobile menu
+const burger = document.getElementById('burger');
+
+const mobileMenu = document.getElementById('mobile-menu');
+
+burger.addEventListener('click', (e) => {
+    if (mobileMenu.classList.contains('active')) {
+        mobileMenu.classList.remove('active')
+        burger.classList.remove('active')
+        return;
+    }
+    mobileMenu.classList.add('active');
+    burger.classList.add('active');
+});
